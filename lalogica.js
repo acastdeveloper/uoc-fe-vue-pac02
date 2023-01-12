69e527f882254d7eada6d15a3b54e4ca6f30050b
acastdeveloper <ac.desenvolupador@gmail.com>
2023-01-12 02:08:03 +0100

    PAC2-FE-VUE-UOC_2023-01-12-2:07AM - Iteracio completada

--- lalogica.js
+++ lalogica.js
@@ -8,7 +8,13 @@ const rMax = 50; // Rang màxim
 // global scope
 let x; // variable de recollida prompt
 let suma; // suma de valors introduïts
-let lM; // longitud de M
+let lM; // longitud d'M
+let sM; // primer element d'M
+let eM; // darrer element d'M
+let inf; // element més petit d'M
+let sup; // element més gran d'M
+let direcc; // direcció per representar resultats d'exercici 4, 5 i 6
+
 
 //arrays
 const M = []; // matriu on s'emmagatzemen els valors introduïts per prompt
@@ -16,6 +22,11 @@ let subPriM = []; // submatriu amb els primers d'M
 let subNoPriM = []; // submatriu de no primers d'M
 let subParM = []; //submatriu amb els elements parells d'M
 let subNoParM = []; // submatriu de no parells d'M
+// arrays de rangs
+const rangM = []; // matriu rang
+let invRangM = []; // matriu de rang inversa
+let rangMPar = []; // Submatriu ascendent de parells de rangMar
+let invRangMSen = []; // Submatriu descendent de senars d'invRangMar
 
 // counters
 let countP; // comptador de prompts i cursor d'M
@@ -33,8 +44,7 @@ let msg1 = ""; // Exercici 1
 let msg2 = ""; // Exercici 2
 let msg3 = ""; // Exercici 3
 let msg4 = ""; // Exercici 4
-let msg5 = ""; // Exercici 5
-let msg6 = ""; // Exercici 6
+let msgTotal = ""; // Missatge complert
 
 /* ****** E N D - V A R I A B L E S ****** */
 
@@ -110,12 +120,28 @@ function sumar(n) { // suma
     suma = eval(M.join("+"));
 }
 
-function analitzar(n) { // Analitza la matriu
+function analitzar(n) { // Analitza la matriu M i els seus elements
     lM = M.length;
+
+    // N'extreu submatrius
     subPriM = M.filter(element => isPri(element)); // submatriu de primers
     subNoPriM = M.filter(element => !isPri(element)); // submatriu de no primers
     subParM = M.filter(element => isPar(element)); // submatriu de parells
     subNoParM = M.filter(element => !isPar(element)); // submatriu de senars
+
+    // Troba cotes
+    sM = M[0]; // Primer número introduït
+    eM = M[(lM - 1)]; // Darrer número intruduït
+    direcc = (sM == eM) ? "equal" : (sM < eM) ? "up" : "down";
+    inf = Math.min(...M);
+    sup = Math.max(...M);
+
+    // Matrius rangM i invRangM
+    for (a = inf; a <= sup; a++) { rangM.push(a); }
+    invRangM = [...rangM].reverse();
+
+    rangMPar = rangM.filter(element => (element % 2 == 0));
+    invRangMSen = invRangM.filter(element => (element % 2 != 0));
 }
 
 function p() { // Funció directriu de processos
@@ -125,62 +151,88 @@ function p() { // Funció directriu de processos
 
 /* -   - O U T P U T -   - */
 function oError(e) { // Mostra l'error que li passem per paràmetre
+    //Exercici 7
     alert(e);
 }
 
 function compMsgs() { // Composició de missatges d'Output
     // Exercici 1
-    msg1 = `La suma és: ${suma}. \n`;
+    msg1 = `La suma és: ${suma}.`;
 
     // Exercici 2
-    if (subPriM.length == lM) { msg2 = ``; }
+    let msg2a = " ";
+    let msg2b = " ";
+    if (subPriM.length == lM) { msg2a = `Tots els nombres introduïts [${subPriM}] són primers.`; }
+    else if (subPriM.length == 0) { msg2a = `Cap dels nombres introduïts és primer`; }
+    else if (subPriM.length == 1) { msg2a = `El nombre [${subPriM}] és primer`; }
+    else { msg2a = `Els nombres [${subPriM}] són primers`; }
 
+    if (subNoPriM.length == 1) { msg2b = ` i el nombre [${subNoPriM}] no és primer.`; }
+    else if (subNoPriM.length > 1) { msg2b = ` i els nombres [${subNoPriM}] no són primers.`; }
+    msg2 = msg2a + msg2b;
 
+    // Exercici 3
+    let msg3a = " ";
+    let msg3b = " ";
+    if (subParM.length == lM) { msg3a = `Tots els nombres introduïts [${subParM}] són parells.`; }
+    else if (subParM.length == 0) { msg3a = `Cap dels nombres introduïts és parell`; }
+    else if (subParM.length == 1) { msg3a = `El nombre [${subParM}] és parell`; }
+    else { msg3a = `Els nombres [${subParM}] són parells`; }
 
-    // Sortida per consola
-    console.log(msg1);
+    if (subNoParM.length == 1) { msg3b = ` i el nombre [${subNoParM}] és senar.`; }
+    else if (subNoParM.length > 1) { msg3b = ` i els nombres [${subNoParM}] són senars.`; }
+    msg3 = msg3a + msg3b;
+
+    // Exercicis 4, 5 i 6
+    msg4 = direcc == "equal" ? inf : direcc == "up" ? rangMPar : invRangMSen;
+
+    msgTotal = `Has introduït: ${M}
+    [Exercici 1] ${msg1}
+    [Exercici 2] ${msg2}
+    [Exercici 3] ${msg3}
+    [Exercici 4, 5 i 6] ${msg4}
+    `;
 }
 
-/* ***** E N D - F U N C T I O N S ***** */
+function oAlert() {
+    alert(msgTotal);
+}
 
 
 
-
-/* ****** M A I N ****** */
-function main() {
+/* -   - M A I N -   - */
+function main() { //Funció d'entrada
     reset();
     r();
     p();
-    // visorDEV();
     compMsgs();
+    oAlert();
+    // visorDEV();
 }
-/* ****** E N D - M A I N ***** */
-
-/* ***** I N T E R A C T I V I T A T ***** */
-window.onload = function () {
-    document.getElementById("start").onclick = main;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* -   - D E V - T E S T S -   - */
 function visorDEV() {
+    // Sortida per consola
+    console.log(`Has introduït: ${M}`);
+    console.log("\n\n");
+    console.log("[Exercici 1]: " + msg1);
+    console.log("\n\n");
+    console.log("[Exercici 2]: " + msg2);
+    console.log("\n\n");
+    console.log("[Exercici 3]: " + msg3);
+    console.log("\n\n");
+    console.log("[Exercicis 4, 5 i 6]: " + msg4);
+
     console.log("x: " + x);
     console.log("Matriu: " + M);
     console.log("Suma: " + suma);
     console.log("Matriu Primers: ", subPriM);
     console.log("Matriu No Primers: ", subNoPriM);
+    console.log(sM, eM, inf, sup);
+}
+
+
+/* ***** I N T E R A C T I V I T A T ***** */
+window.onload = function () {
+    document.getElementById("start").onclick = main;
 }
